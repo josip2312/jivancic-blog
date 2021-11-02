fb6507a697a760699ad28e70268b2ba47591b7a0
Josip Ivancic <ivancic.josip@nsoft.com>
2021-11-02 10:21:54 +0100

    Add sass setup

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import DefaultTheme from "vitepress/theme";
+import DarkMode from "./DarkMode.vue";
+import AppNavigation from "@/components/AppNavigation.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <div class="index">
+    <Layout>
+      <template
+        #sidebar-top
+        v-if="!$frontmatter.sidebar && !$frontmatter.author"
+      >
+        <AppNavigation />
+      </template>
+      <template #navbar-search>
+        <AppNavigation class="desktop-nav" />
+        <DarkMode />
+      </template>
+    </Layout>
+    <TheFooter />
+  </div>
+</template>
+
+<style lang="scss">
+.desktop-nav {
+  display: none;
+
+  @media screen and(min-width: 720px) {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.index {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,9 @@
+import DefaultTheme from "vitepress/theme";
+import Layout from "./Layout.vue";
+import "@/styles/main.scss";
+
+export default {
+  ...DefaultTheme,
+  Layout,
+  enhanceApp({ app }) {},
+};
