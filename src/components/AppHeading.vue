784488a070751dabe19ab463fde783ea5173dc73
Josip Ivancic <josip.ivancic23@gmail.com>
2022-08-28 11:36:27 +0200

    Add AppHeading component

---
+++ src/components/AppHeading.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="page-title">
+    <slot />
+  </h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.page-title {
+  font-size: clamp(2rem, 4vw, 2.75rem);
+  font-weight: 700;
+  margin-bottom: 1.5rem;
+
+  @include mq-small {
+    margin-bottom: 2rem;
+  }
+}
+</style>
